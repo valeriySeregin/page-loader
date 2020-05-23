d4c4f412219a45d633db750faa9a9d9ac90241e4
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-05-23 18:21:47 +0300

    fix pageload.js

--- src/bin/pageload.js
+++ src/bin/pageload.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import getHTMLPage from '..';
 
 program
-  .version('0.0.0')
+  .version('0.0.1')
   .description('Downloads page from internet and saves it in specified directory')
-  .arguments('<directory> <URL>')
-  .action(() => {});
+  .option('-o, --output [type]', 'destination directory', process.cwd())
+  .arguments('<URL>')
+  .action((url, options) => {
+    getHTMLPage(options.output, url);
+    console.log(`Page from ${url} downloaded successfully!`);
+  });
 
 program.parse(process.argv);
