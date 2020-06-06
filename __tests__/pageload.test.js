8059d1067b564e0381c1d1776e91558021102576
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-06-06 18:08:49 +0300

    add tests for downloading local resources

--- __tests__/pageload.test.js
+++ __tests__/pageload.test.js
@@ -2,35 +2,88 @@ import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
 import nock from 'nock';
-import getHTMLPage from '../src';
+import {
+  downloadPage,
+  downloadResource,
+  changeResourcesLinks,
+} from '../src/utils';
 
-const getPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
 let tmpdirPath;
 
-beforeEach(async () => {
+beforeAll(async () => {
   tmpdirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('download and save page', async () => {
+test('download page', async () => {
   try {
-    const expected = await fs.readFile(getPath('hexlet-io-courses.html'), 'utf-8');
+    const response = await fs.readFile(getFixturePath('localhost-test.html'), 'utf-8');
 
-    nock('https://hexlet.io')
-      .get('/courses')
-      .reply(200, expected);
+    nock('http://localhost')
+      .get('/test')
+      .reply(200, response);
 
-    await getHTMLPage(tmpdirPath, 'https://hexlet.io/courses');
-    const loadedPage = await fs.readFile(path.join(tmpdirPath, 'hexlet-io-courses.html'), 'utf-8');
+    await downloadPage(tmpdirPath, 'http://localhost/test');
+    const page = await fs.readFile(path.join(tmpdirPath, 'localhost-test.html'), 'utf-8');
 
-    expect(loadedPage).toEqual(expected);
+    expect(page).toEqual(response);
   } catch (e) {
-    console.log(e);
+    console.error(e);
     throw e;
   }
 });
 
-afterEach(async () => {
-  await fs.unlink(path.join(tmpdirPath, 'hexlet-io-courses.html'));
-  await fs.rmdir(tmpdirPath);
+test('download local resource 1', async () => {
+  try {
+    const response = await fs.readFile(getFixturePath('image.png'), { encoding: 'utf8' });
+
+    nock('http://localhost')
+      .get('/test/image.png')
+      .reply(200, response);
+
+    await downloadResource(tmpdirPath, 'http://localhost/test', 'image.png');
+    const resource = await fs.readFile(path.join(tmpdirPath, 'localhost-test_files/image.png'), 'utf-8');
+
+    expect(resource).toEqual(response);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+});
+
+test('download local resource 2', async () => {
+  try {
+    const response = await fs.readFile(getFixturePath('style.css'), { encoding: 'utf8' });
+
+    nock('http://localhost')
+      .get('/test/style.css')
+      .reply(200, response);
+
+    await downloadResource(tmpdirPath, 'http://localhost/test', 'style.css');
+    const resource = await fs.readFile(path.join(tmpdirPath, 'localhost-test_files/style.css'), 'utf-8');
+
+    expect(resource).toEqual(response);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+});
+
+test('change resources links on page', async () => {
+  try {
+    const expected = await fs.readFile(getFixturePath('localhost-test-changed.html'), 'utf-8');
+    await changeResourcesLinks(path.join(tmpdirPath, 'localhost-test.html'), 'http://localhost/test');
+    const changedPage = await fs.readFile(path.join(tmpdirPath, 'localhost-test.html'), 'utf-8');
+
+    expect(changedPage).toEqual(expected);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+});
+
+afterAll(async () => {
+  await fs.unlink(path.join(tmpdirPath, 'localhost-test.html'));
+  await fs.rmdir(tmpdirPath, { recursive: true });
 });
