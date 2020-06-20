b971e34a26223cbd83bc28c15631b8a37e12f740
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-06-20 18:24:46 +0300

    update version

--- src/bin/pageload.js
+++ src/bin/pageload.js
@@ -4,13 +4,12 @@ import program from 'commander';
 import getHTMLPage from '..';
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .description('Downloads page from internet and saves it in specified directory')
   .option('-o, --output [type]', 'destination directory', process.cwd())
   .arguments('<URL>')
   .action((url, options) => {
     getHTMLPage(options.output, url);
-    console.log(`Page from ${url} downloaded successfully!`);
   });
 
 program.parse(process.argv);
