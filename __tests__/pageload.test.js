9bad5e4e43efa82c96bfebfd31729863171aad8d
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-06-24 15:27:26 +0300

    change unit testing with integration testing

--- __tests__/pageload.test.js
+++ __tests__/pageload.test.js
@@ -2,12 +2,7 @@ import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
 import nock from 'nock';
-import {
-  downloadPage,
-  downloadResource,
-  changeResourcesLinks,
-  getLinksOfLocalResources,
-} from '../src';
+import getHTMLPage from '../src';
 
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
@@ -17,68 +12,39 @@ beforeAll(async () => {
   tmpdirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('download page', async () => {
+test('download HTML page with local resources', async () => {
   try {
     const response = await fs.readFile(getFixturePath('localhost-test.html'), 'utf-8');
+    const expected = await fs.readFile(getFixturePath('localhost-test-changed.html'), 'utf-8');
 
     nock('http://localhost')
       .get('/')
       .reply(200, response);
 
-    await downloadPage(tmpdirPath, 'http://localhost');
+    await getHTMLPage(tmpdirPath, 'http://localhost');
     const page = await fs.readFile(path.join(tmpdirPath, 'localhost.html'), 'utf-8');
 
-    expect(page).toEqual(response);
+    expect(page).toEqual(expected);
   } catch (e) {
     console.error(e);
     throw e;
   }
 });
 
-test.each([
-  ['style.css'],
-  ['image.png'],
-])('downloadLocalResource(%s)', async (link) => {
-  try {
-    const response = await fs.readFile(getFixturePath(link), { encoding: 'utf8' });
+test('downloading fails on HTTP error 404', async () => {
+  nock('https://somename.smt')
+    .get('/')
+    .reply(404);
 
-    nock('http://localhost')
-      .get(`/${link}`)
-      .reply(200, response);
-
-    await downloadResource(tmpdirPath, 'http://localhost', link);
-    const resource = await fs.readFile(path.join(tmpdirPath, `localhost_files/${link}`), 'utf-8');
-
-    expect(resource).toEqual(response);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+  await expect(getHTMLPage('mySuperDirectory', 'https://somename.smt')).rejects.toThrow(
+    'Request failed with status code 404',
+  );
 });
 
-test('get links of local resources', async () => {
-  try {
-    const expected = ['style.css', 'image.png'];
-    const resourcesLinks = await getLinksOfLocalResources(tmpdirPath, 'http://localhost');
-
-    expect(resourcesLinks).toEqual(expected);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-});
-
-test('change resources links on page', async () => {
-  try {
-    const expected = await fs.readFile(getFixturePath('localhost-test-changed.html'), 'utf-8');
-    await changeResourcesLinks(tmpdirPath, 'http://localhost');
-    const changedPage = await fs.readFile(path.join(tmpdirPath, 'localhost.html'), 'utf-8');
-
-    expect(changedPage).toEqual(expected);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+test('downloading fails on incorrect directory', async () => {
+  await expect(getHTMLPage('mySuperDirectory', 'https://hexlet.io')).rejects.toThrow(
+    'ENOENT: no such file or directory, mkdir \'mySuperDirectory/hexlet-io_files\'',
+  );
 });
 
 afterAll(async () => {
