b344d9fb28513dcc872fb2851fbcb2de9e3fd498
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-06-26 17:37:42 +0300

    change tests

--- __tests__/pageload.test.js
+++ __tests__/pageload.test.js
@@ -2,52 +2,80 @@ import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
 import nock from 'nock';
+import { URL } from 'url';
 import getHTMLPage from '../src';
 
-const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 let tmpdirPath;
 
-beforeAll(async () => {
+beforeEach(async () => {
   tmpdirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  nock.disableNetConnect();
 });
 
 test('download HTML page with local resources', async () => {
-  try {
-    const response = await fs.readFile(getFixturePath('localhost-test.html'), 'utf-8');
-    const expected = await fs.readFile(getFixturePath('localhost-test-changed.html'), 'utf-8');
+  const testURL = new URL('http://localhost.loc/resources');
+  const pageResponse = await fs.readFile(getFixturePath('localhost-test.html'), 'utf-8');
 
-    nock('http://localhost')
-      .get('/')
-      .reply(200, response);
+  nock(testURL.origin)
+    .get(testURL.pathname)
+    .reply(200, pageResponse)
+    .log(console.log);
 
-    await getHTMLPage(tmpdirPath, 'http://localhost');
-    const page = await fs.readFile(path.join(tmpdirPath, 'localhost.html'), 'utf-8');
+  const firstResourceResponse = await fs.readFile(getFixturePath('style.css'), 'utf-8');
 
-    expect(page).toEqual(expected);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+  nock(testURL.origin)
+    .get(`${testURL.pathname}/style.css`)
+    .reply(200, firstResourceResponse)
+    .log(console.log);
+
+  const secondResourceResponse = await fs.readFile(getFixturePath('image.png'), 'utf-8');
+
+  nock(testURL.origin)
+    .get(`${testURL.pathname}/image.png`)
+    .reply(200, secondResourceResponse)
+    .log(console.log);
+
+  await getHTMLPage(tmpdirPath, testURL.href);
+
+  const actual1 = await fs.readFile(path.join(tmpdirPath, 'localhost-loc-resources.html'), 'utf-8');
+  const expected1 = await fs.readFile(getFixturePath('localhost-test-changed.html'), 'utf-8');
+
+  const actual2 = await fs.readFile(path.join(tmpdirPath, 'localhost-loc-resources_files', 'style.css'), 'utf-8');
+  const expected2 = firstResourceResponse;
+
+  const actual3 = await fs.readFile(path.join(tmpdirPath, 'localhost-loc-resources_files', 'image.png'), 'utf-8');
+  const expected3 = secondResourceResponse;
+
+  expect(actual1).toEqual(expected1);
+  expect(actual2).toEqual(expected2);
+  expect(actual3).toEqual(expected3);
 });
 
 test('downloading fails on HTTP error 404', async () => {
-  nock('https://somename.smt')
-    .get('/')
-    .reply(404);
+  const testURL = new URL('https://somename.smt');
 
-  await expect(getHTMLPage('mySuperDirectory', 'https://somename.smt')).rejects.toThrow(
-    'Request failed with status code 404',
-  );
+  nock(testURL.origin)
+    .get(testURL.pathname)
+    .reply(404)
+    .log(console.log);
+
+  await expect(getHTMLPage('/tmp', testURL.href)).rejects.toThrow();
 });
 
 test('downloading fails on incorrect directory', async () => {
-  await expect(getHTMLPage('mySuperDirectory', 'https://hexlet.io')).rejects.toThrow(
-    'ENOENT: no such file or directory, mkdir \'mySuperDirectory/hexlet-io_files\'',
-  );
+  const testURL = new URL('https://somename.smt');
+
+  nock(testURL.origin)
+    .get(testURL.pathname)
+    .reply(200)
+    .log(console.log);
+
+  await expect(getHTMLPage('/mySuperDirectory', testURL.href)).rejects.toThrow();
 });
 
-afterAll(async () => {
-  await fs.unlink(path.join(tmpdirPath, 'localhost.html'));
+afterEach(async () => {
   await fs.rmdir(tmpdirPath, { recursive: true });
+  nock.enableNetConnect();
 });
