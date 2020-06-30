066c780895e507abaeb1b30949b1644f17d6b9ee
Valeriy Seregin <40579575+valeriySeregin@users.noreply.github.com>
2020-06-30 20:04:58 +0300

    divide main logic and side effects

--- src/index.js
+++ src/index.js
@@ -22,93 +22,70 @@ const getName = (link, type = 'file') => {
   return name[type];
 };
 
-const downloadPage = (dirpath, url, pagename, dirname) => {
-  let html;
+const getLinksOfLocalResources = (html) => {
+  const $ = cheerio.load(html, { decodeEntities: false });
+  const links = $('link, script, img')
+    .toArray()
+    .map((element) => $(element).attr('href') || $(element).attr('src'))
+    .filter((element) => element)
+    .filter((link) => link.split('//').length === 1);
 
-  debug(`Download page from ${url}`);
+  debug('Extracted local links:', links);
 
-  return axios(url)
-    .then(({ data }) => {
-      html = data;
-    })
-    .then(() => fs.mkdir(path.join(dirpath, dirname)))
-    .then(() => fs.writeFile(path.join(dirpath, pagename), html));
+  return links;
 };
 
-const getLinksOfLocalResources = (dirpath, pagename) => {
-  const filepath = `${path.join(dirpath, pagename)}`;
-
-  return fs.readFile(filepath, 'utf-8')
-    .then((html) => {
-      const $ = cheerio.load(html, { decodeEntities: false });
-      const links = $('link, script, img')
-        .toArray()
-        .map((element) => $(element).attr('href') || $(element).attr('src'))
-        .filter((element) => element)
-        .filter((link) => link.split('//').length === 1);
-
-      debug('Extracted local links:', links);
-
-      return links;
-    });
-};
-
-const downloadResource = (dirpath, url, link, dirname) => {
-  const filename = getName(link);
+const downloadResource = (url, link) => {
   const downloadingLink = new URL(url);
-  downloadingLink.pathname = `${downloadingLink.pathname}/${link}`;
-  let resource;
+  downloadingLink.pathname = path.join(downloadingLink.pathname, link);
 
   debug(`Download resource from ${downloadingLink.href}`);
 
-  return axios(downloadingLink.href)
-    .then(({ data }) => {
-      resource = data;
-    })
-    .then(() => fs.writeFile(path.join(dirpath, dirname, filename), resource));
+  return axios(downloadingLink.href);
 };
 
-const changeResourcesLinks = (dirpath, pagename, dirname) => fs.readFile(`${path.join(dirpath, pagename)}`, 'utf-8')
-  .then((html) => {
-    debug(`File from ${dirpath} has been read`);
-    const $ = cheerio.load(html, { xmlMode: true, decodeEntities: false });
-    $('link, script, img')
-      .each((i, tag) => {
-        const mapping = {
-          img: 'src',
-          link: 'href',
-          script: 'src',
-        };
-        const oldAttr = $(tag).attr('href') || $(tag).attr('src');
-        const attrToChange = mapping[tag.name];
-        const value = oldAttr.split('//').length === 1 ? `${path.join(dirname, getName(oldAttr))}` : oldAttr;
-        $(tag).attr(attrToChange, value);
-      });
-    fs.writeFile(`${path.join(dirpath, pagename)}`, $.html());
-  });
+const changeResourcesLinks = (html, dirname) => {
+  const $ = cheerio.load(html, { xmlMode: true, decodeEntities: false });
+  $('link, script, img')
+    .each((i, tag) => {
+      const mapping = {
+        img: 'src',
+        link: 'href',
+        script: 'src',
+      };
+      const oldAttr = $(tag).attr('href') || $(tag).attr('src');
+      const attrToChange = mapping[tag.name];
+      const value = oldAttr.split('//').length === 1 ? `${path.join(dirname, getName(oldAttr))}` : oldAttr;
+      $(tag).attr(attrToChange, value);
+    });
+
+  return $;
+};
 
 export default (dirpath, url) => {
   const pagename = getName(url, 'page');
   const dirname = getName(url, 'directory');
 
-  const tasks = new Listr([
-    {
-      title: 'Download HTML page',
-      task: () => downloadPage(dirpath, url, pagename, dirname),
-    },
-    {
-      title: 'Get local resources links and download resources',
-      task: () => getLinksOfLocalResources(dirpath, pagename)
-        .then((links) => links.map((link) => downloadResource(dirpath, url, link, dirname)
-          .then((v) => ({ result: 'success', value: v }))
-          .catch((e) => ({ result: 'error', error: e }))))
-        .then((promises) => Promise.all(promises)),
-    },
-    {
-      title: 'Change resources links',
-      task: () => changeResourcesLinks(dirpath, pagename, dirname),
-    },
-  ]);
+  let linksOnLocalResources;
 
-  return tasks.run();
+  debug(`Request to ${url}`);
+
+  return axios(url)
+    .then((response) => fs.writeFile(path.join(dirpath, pagename), response.data))
+    .then(() => fs.mkdir(path.join(dirpath, dirname)))
+    .then(() => fs.readFile(path.join(dirpath, pagename), 'utf-8'))
+    .then((html) => getLinksOfLocalResources(html))
+    .then((links) => {
+      linksOnLocalResources = links;
+      const promises = links.map((link) => downloadResource(url, link));
+      return Promise.all(promises);
+    })
+    .then((resources) => {
+      const promises = resources.map(({ data }, i) => fs
+        .writeFile(path.join(dirpath, dirname, getName(linksOnLocalResources[i])), data));
+      return Promise.all(promises);
+    })
+    .then(() => fs.readFile(`${path.join(dirpath, pagename)}`, 'utf-8'))
+    .then((html) => changeResourcesLinks(html, dirname))
+    .then(($) => fs.writeFile(`${path.join(dirpath, pagename)}`, $.html()));
 };
